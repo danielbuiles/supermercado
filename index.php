f2acfb7ec4d864fef15b70f84928a893e129e32c
danielbuiles <61318415+danielbuiles@users.noreply.github.com>
2020-09-11 07:45:47 -0500

    index.html
    es el index la pagina principal

---
+++ index.php
@@ -0,0 +1,29 @@
+<?
+ $producto1="arroz";
+ $producto2="carne";
+ $producto3="arepas";
+ $producto4="huevos";
+ $producto5="cofee";
+ $producto6="chocolate";
+ $producto7="papa";
+ $producto8="queso";
+ $producto9="manzana";
+ $producto10="frijoles";
+
+ $precio1=5000;
+ $precio2=22000;
+ $precio3=6000;
+ $precio4=11000;
+ $precio5=7500;
+ $precio6=6000;
+ $precio7=5000;
+ $precio8=8000;
+ $precio9=8000;
+ $precio10=7000;
+
+ $total($precio1+$precio2+$precio3+$precio4+$precio5+$precio6+$precio7+$precio8+$precio9+$precio10);
+ 
+ echo($total);
+
+
+?>
